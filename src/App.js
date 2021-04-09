6f30f5ced138aa8b12a1190e4f4465b671834ea4
milenablazheva <milenablajeva@abv.bg>
2021-04-09 15:33:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState} from "react";
+import "./styles.css";
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+fetch("https://randomuser.me/api/?results=3")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    setContacts(data.results);
+  });
+
+  return (
+    <>
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
+    </>
+  );
+};
+
+  const ContactCard = props => {
+    const [showAge, setShowAge] = useState(false);
+    console.log(props.avatar); 
+	  console.log(props.name);
+	  console.log(props.email);
+	  console.log(props.age);
+
+    
+  return (
+    
+    <div className = "contact-card">
+      
+	<img src = {props.avatar} alt = "profile" />
+
+	<div className = "user-details">
+		<p>Name: {props.name} </p>
+		<p>Email: {props.email} </p>
+    
+    <button onClick={() => setShowAge(!showAge)}>
+	  Toggle Age 
+    </button>
+		{showAge === true ? <p>Age: {props.age} </p> : null}
+	</div>
+</div>
+  )
+}
+
+export default App;
